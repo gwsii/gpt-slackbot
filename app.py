cada2599d999e2989f2a716cb522126f292fb97e
Greg Smith <grgrsmithii@gmail.com>
2023-11-15 00:18:51 -0600

    try out respond with DMs

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ app = App(
 def handle_direct_message_events(ack, say, event, respond):
     print(event)
     ack()
-    sleep(10)
-    if 'thread_ts' in event:
-        say(f"Thanks for your message: {event['text']}", thread_ts=event['thread_ts'])
-    else:
-        say(f"Thanks for your message: {event['text']}", thread_ts=event['ts'])
+    # sleep(10)
+    # if 'thread_ts' in event:
+    #     say(f"Thanks for your message: {event['text']}", thread_ts=event['thread_ts'])
+    # else:
+    #     say(f"Thanks for your message: {event['text']}", thread_ts=event['ts'])
+    respond(f"Thanks for your message: {event['text']}")
 
 @app.event("app_mention")
 def handle_app_mentions(ack, say, event, respond):
