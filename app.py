cd47a16ff4ba1818e912c65707c04f4c9644a9ac
Greg Smith <grgrsmithii@gmail.com>
2023-11-14 20:26:12 -0600

    initial version

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 app = Flask(__name__)
+FlaskJSON(app)
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/get_time')
+def get_time():
+    return json_response(time='now')
+
+@app.route('/increment', methods=['POST'])
+def increment():
+    data = request.get_json(force=True)
+    if 'number' not in data:
+        raise JsonError(description='Invalid input', code=400)
+    return json_response(result=data['number'] + 1)
+
+@app.route('/get_value')
+@as_json
+def get_value():
+    return dict(value=42)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
